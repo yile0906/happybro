b165b2e79e7e1d976f6820b9ce9ed190f951b2a1
yile0906 <ckxlovezqq@gmail.com>
2019-05-09 17:05:52 +0800

    5.9

--- hb-backend/registry/src/main/java/com/yile/RegistryApplication.java
+++ hb-backend/registry/src/main/java/com/yile/RegistryApplication.java
@@ -2,12 +2,13 @@ package com.yile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistryApplication {
 
     public static void main(String[] args) {
-        System.out.println("1111");
         SpringApplication.run(RegistryApplication.class, args);
     }
 
